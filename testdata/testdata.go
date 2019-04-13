6a7d473d0c520408d3d975ce4078681580902a80
Jan Vornberger <jan@uos.de>
2019-04-13 21:03:53 +0200

    Improve go code

--- testdata/testdata.go
+++ testdata/testdata.go
@@ -1,22 +1,25 @@
 package main
 
 import (
-    "crypto/rand"
-    "encoding/hex"
-    "flag"
-    "fmt"
+	"crypto/rand"
+	"encoding/hex"
+	"flag"
+	"fmt"
+	"log"
 
-    "golang.org/x/crypto/curve25519"
+	"golang.org/x/crypto/curve25519"
 )
 
 func main() {
-    var n = flag.Int("n", 10, "number of test cases to generate")
-    flag.Parse()
-
-    var in, dst [32]byte
-    for i := 0; i < *n; i++ {
-        rand.Read(in[:])
-        curve25519.ScalarBaseMult(&dst, &in)
-        fmt.Printf("%s:%s\n", hex.EncodeToString(in[:]), hex.EncodeToString(dst[:]))
-    }
+	var n = flag.Int("n", 10, "number of test cases to generate")
+	flag.Parse()
+	var in, dst [32]byte
+	for i := 0; i < *n; i++ {
+		_, err := rand.Read(in[:])
+		if err != nil {
+			log.Fatal(err)
+		}
+		curve25519.ScalarBaseMult(&dst, &in)
+		fmt.Printf("%s:%s\n", hex.EncodeToString(in[:]), hex.EncodeToString(dst[:]))
+	}
 }
