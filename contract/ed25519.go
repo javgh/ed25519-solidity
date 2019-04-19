c74bd38f747ca59f165b38b96f180ba449e7b251
Jan Vornberger <jan@uos.de>
2019-04-19 10:26:54 +0200

    Improve testsuite and remove unused code

---
+++ contract/ed25519.go
@@ -0,0 +1,245 @@
+// Code generated - DO NOT EDIT.
+// This file is a generated binding and any manual changes will be lost.
+
+package ed25519
+
+import (
+	"math/big"
+	"strings"
+
+	ethereum "github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/event"
+)
+
+// Reference imports to suppress errors if they are not otherwise used.
+var (
+	_ = big.NewInt
+	_ = strings.NewReader
+	_ = ethereum.NotFound
+	_ = abi.U256
+	_ = bind.Bind
+	_ = common.Big1
+	_ = types.BloomLookup
+	_ = event.NewSubscription
+)
+
+// Ed25519ABI is the input ABI used to generate the binding from.
+const Ed25519ABI = "[{\"constant\":true,\"inputs\":[],\"name\":\"gasEstimation\",\"outputs\":[{\"name\":\"\",\"type\":\"uint256\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[{\"name\":\"s\",\"type\":\"uint256\"}],\"name\":\"scalarmult\",\"outputs\":[{\"name\":\"\",\"type\":\"uint256\"},{\"name\":\"\",\"type\":\"uint256\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"}]"
+
+// Ed25519Bin is the compiled bytecode used for deploying new contracts.
+const Ed25519Bin = `608060405234801561001057600080fd5b5061057a806100206000396000f3fe608060405234801561001057600080fd5b5060043610610052577c010000000000000000000000000000000000000000000000000000000060003504634f9b9e398114610057578063e49cf91114610071575b600080fd5b61005f6100a7565b60408051918252519081900360200190f35b61008e6004803603602081101561008757600080fd5b50356100da565b6040805192835260208301919091528051918290030190f35b6000806100d37f7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff6100da565b9250505090565b6000806100e56104e6565b6100ed6104e6565b7f216936d3cd6e53fec0a4e231fdd6dc5c692cc7609525a7b2c9562d608f25d51a82527f66666666666666666666666666666666666666666666666666666666666666586020808401919091526001604080850182905260008452918301819052908201525b600085111561018b5784600116600114156101755761017281836101cf565b90505b6002909404936101848261034d565b9150610153565b600061019a8260400151610484565b9050601360ff60020a0382518290098252601360ff60020a038183602001510960208301819052915194509092505050915091565b6101d76104e6565b6101df610508565b601360ff60020a0383604001518560400151098152601360ff60020a03815180096020820152601360ff60020a0383518551096040820152601360ff60020a0383602001518560200151096060820152601360ff60020a038082606001518360400151097f52036cee2b6ffe738cc740797779e89800700a4d4141d8ab75eb4dca135978a3096080820152601360ff60020a038160800151601360ff60020a030382602001510860a0820152601360ff60020a03816080015182602001510860c0820152601360ff60020a03806060830151601360ff60020a03908103906040850151601360ff60020a0390810390601360ff60020a0360208a01518a5108601360ff60020a0360208c01518c5108090808601360ff60020a0360a0840151845109098252601360ff60020a03808260400151836060015108601360ff60020a0360c0840151845109096020830152601360ff60020a038160c001518260a001510960408301525092915050565b6103556104e6565b61035d610508565b601360ff60020a0360208401518451088152601360ff60020a03815180096020820152601360ff60020a03835180096040820152601360ff60020a0360208401518009606082018190526040820151601360ff60020a03908103608084018190529091900860a0820152601360ff60020a036040840151800960e0820152601360ff60020a03808260e00151600209601360ff60020a03038260a001510860c0820152601360ff60020a0360c0820151601360ff60020a036060840151601360ff60020a03908103908560400151601360ff60020a030386602001510808098252601360ff60020a03808260600151601360ff60020a03038360800151088260a00151096020830152601360ff60020a038160c001518260a0015109604083015250919050565b6000806002601360ff60020a030390506000601360ff60020a03905060405160208152602080820152602060408201528460608201528260808201528160a082015260208160c0836005600019fa15156104dd57600080fd5b51949350505050565b6060604051908101604052806000815260200160008152602001600081525090565b610100604051908101604052806000815260200160008152602001600081526020016000815260200160008152602001600081526020016000815260200160008152509056fea165627a7a723058202b61caa2a00950c1c2104d9366aa232c17d92e448cf44b16eef1f669fb052d410029`
+
+// DeployEd25519 deploys a new Ethereum contract, binding an instance of Ed25519 to it.
+func DeployEd25519(auth *bind.TransactOpts, backend bind.ContractBackend) (common.Address, *types.Transaction, *Ed25519, error) {
+	parsed, err := abi.JSON(strings.NewReader(Ed25519ABI))
+	if err != nil {
+		return common.Address{}, nil, nil, err
+	}
+	address, tx, contract, err := bind.DeployContract(auth, parsed, common.FromHex(Ed25519Bin), backend)
+	if err != nil {
+		return common.Address{}, nil, nil, err
+	}
+	return address, tx, &Ed25519{Ed25519Caller: Ed25519Caller{contract: contract}, Ed25519Transactor: Ed25519Transactor{contract: contract}, Ed25519Filterer: Ed25519Filterer{contract: contract}}, nil
+}
+
+// Ed25519 is an auto generated Go binding around an Ethereum contract.
+type Ed25519 struct {
+	Ed25519Caller     // Read-only binding to the contract
+	Ed25519Transactor // Write-only binding to the contract
+	Ed25519Filterer   // Log filterer for contract events
+}
+
+// Ed25519Caller is an auto generated read-only Go binding around an Ethereum contract.
+type Ed25519Caller struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// Ed25519Transactor is an auto generated write-only Go binding around an Ethereum contract.
+type Ed25519Transactor struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// Ed25519Filterer is an auto generated log filtering Go binding around an Ethereum contract events.
+type Ed25519Filterer struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// Ed25519Session is an auto generated Go binding around an Ethereum contract,
+// with pre-set call and transact options.
+type Ed25519Session struct {
+	Contract     *Ed25519          // Generic contract binding to set the session for
+	CallOpts     bind.CallOpts     // Call options to use throughout this session
+	TransactOpts bind.TransactOpts // Transaction auth options to use throughout this session
+}
+
+// Ed25519CallerSession is an auto generated read-only Go binding around an Ethereum contract,
+// with pre-set call options.
+type Ed25519CallerSession struct {
+	Contract *Ed25519Caller // Generic contract caller binding to set the session for
+	CallOpts bind.CallOpts  // Call options to use throughout this session
+}
+
+// Ed25519TransactorSession is an auto generated write-only Go binding around an Ethereum contract,
+// with pre-set transact options.
+type Ed25519TransactorSession struct {
+	Contract     *Ed25519Transactor // Generic contract transactor binding to set the session for
+	TransactOpts bind.TransactOpts  // Transaction auth options to use throughout this session
+}
+
+// Ed25519Raw is an auto generated low-level Go binding around an Ethereum contract.
+type Ed25519Raw struct {
+	Contract *Ed25519 // Generic contract binding to access the raw methods on
+}
+
+// Ed25519CallerRaw is an auto generated low-level read-only Go binding around an Ethereum contract.
+type Ed25519CallerRaw struct {
+	Contract *Ed25519Caller // Generic read-only contract binding to access the raw methods on
+}
+
+// Ed25519TransactorRaw is an auto generated low-level write-only Go binding around an Ethereum contract.
+type Ed25519TransactorRaw struct {
+	Contract *Ed25519Transactor // Generic write-only contract binding to access the raw methods on
+}
+
+// NewEd25519 creates a new instance of Ed25519, bound to a specific deployed contract.
+func NewEd25519(address common.Address, backend bind.ContractBackend) (*Ed25519, error) {
+	contract, err := bindEd25519(address, backend, backend, backend)
+	if err != nil {
+		return nil, err
+	}
+	return &Ed25519{Ed25519Caller: Ed25519Caller{contract: contract}, Ed25519Transactor: Ed25519Transactor{contract: contract}, Ed25519Filterer: Ed25519Filterer{contract: contract}}, nil
+}
+
+// NewEd25519Caller creates a new read-only instance of Ed25519, bound to a specific deployed contract.
+func NewEd25519Caller(address common.Address, caller bind.ContractCaller) (*Ed25519Caller, error) {
+	contract, err := bindEd25519(address, caller, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &Ed25519Caller{contract: contract}, nil
+}
+
+// NewEd25519Transactor creates a new write-only instance of Ed25519, bound to a specific deployed contract.
+func NewEd25519Transactor(address common.Address, transactor bind.ContractTransactor) (*Ed25519Transactor, error) {
+	contract, err := bindEd25519(address, nil, transactor, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &Ed25519Transactor{contract: contract}, nil
+}
+
+// NewEd25519Filterer creates a new log filterer instance of Ed25519, bound to a specific deployed contract.
+func NewEd25519Filterer(address common.Address, filterer bind.ContractFilterer) (*Ed25519Filterer, error) {
+	contract, err := bindEd25519(address, nil, nil, filterer)
+	if err != nil {
+		return nil, err
+	}
+	return &Ed25519Filterer{contract: contract}, nil
+}
+
+// bindEd25519 binds a generic wrapper to an already deployed contract.
+func bindEd25519(address common.Address, caller bind.ContractCaller, transactor bind.ContractTransactor, filterer bind.ContractFilterer) (*bind.BoundContract, error) {
+	parsed, err := abi.JSON(strings.NewReader(Ed25519ABI))
+	if err != nil {
+		return nil, err
+	}
+	return bind.NewBoundContract(address, parsed, caller, transactor, filterer), nil
+}
+
+// Call invokes the (constant) contract method with params as input values and
+// sets the output to result. The result type might be a single field for simple
+// returns, a slice of interfaces for anonymous returns and a struct for named
+// returns.
+func (_Ed25519 *Ed25519Raw) Call(opts *bind.CallOpts, result interface{}, method string, params ...interface{}) error {
+	return _Ed25519.Contract.Ed25519Caller.contract.Call(opts, result, method, params...)
+}
+
+// Transfer initiates a plain transaction to move funds to the contract, calling
+// its default method if one is available.
+func (_Ed25519 *Ed25519Raw) Transfer(opts *bind.TransactOpts) (*types.Transaction, error) {
+	return _Ed25519.Contract.Ed25519Transactor.contract.Transfer(opts)
+}
+
+// Transact invokes the (paid) contract method with params as input values.
+func (_Ed25519 *Ed25519Raw) Transact(opts *bind.TransactOpts, method string, params ...interface{}) (*types.Transaction, error) {
+	return _Ed25519.Contract.Ed25519Transactor.contract.Transact(opts, method, params...)
+}
+
+// Call invokes the (constant) contract method with params as input values and
+// sets the output to result. The result type might be a single field for simple
+// returns, a slice of interfaces for anonymous returns and a struct for named
+// returns.
+func (_Ed25519 *Ed25519CallerRaw) Call(opts *bind.CallOpts, result interface{}, method string, params ...interface{}) error {
+	return _Ed25519.Contract.contract.Call(opts, result, method, params...)
+}
+
+// Transfer initiates a plain transaction to move funds to the contract, calling
+// its default method if one is available.
+func (_Ed25519 *Ed25519TransactorRaw) Transfer(opts *bind.TransactOpts) (*types.Transaction, error) {
+	return _Ed25519.Contract.contract.Transfer(opts)
+}
+
+// Transact invokes the (paid) contract method with params as input values.
+func (_Ed25519 *Ed25519TransactorRaw) Transact(opts *bind.TransactOpts, method string, params ...interface{}) (*types.Transaction, error) {
+	return _Ed25519.Contract.contract.Transact(opts, method, params...)
+}
+
+// GasEstimation is a free data retrieval call binding the contract method 0x4f9b9e39.
+//
+// Solidity: function gasEstimation() constant returns(uint256)
+func (_Ed25519 *Ed25519Caller) GasEstimation(opts *bind.CallOpts) (*big.Int, error) {
+	var (
+		ret0 = new(*big.Int)
+	)
+	out := ret0
+	err := _Ed25519.contract.Call(opts, out, "gasEstimation")
+	return *ret0, err
+}
+
+// GasEstimation is a free data retrieval call binding the contract method 0x4f9b9e39.
+//
+// Solidity: function gasEstimation() constant returns(uint256)
+func (_Ed25519 *Ed25519Session) GasEstimation() (*big.Int, error) {
+	return _Ed25519.Contract.GasEstimation(&_Ed25519.CallOpts)
+}
+
+// GasEstimation is a free data retrieval call binding the contract method 0x4f9b9e39.
+//
+// Solidity: function gasEstimation() constant returns(uint256)
+func (_Ed25519 *Ed25519CallerSession) GasEstimation() (*big.Int, error) {
+	return _Ed25519.Contract.GasEstimation(&_Ed25519.CallOpts)
+}
+
+// Scalarmult is a free data retrieval call binding the contract method 0xe49cf911.
+//
+// Solidity: function scalarmult(uint256 s) constant returns(uint256, uint256)
+func (_Ed25519 *Ed25519Caller) Scalarmult(opts *bind.CallOpts, s *big.Int) (*big.Int, *big.Int, error) {
+	var (
+		ret0 = new(*big.Int)
+		ret1 = new(*big.Int)
+	)
+	out := &[]interface{}{
+		ret0,
+		ret1,
+	}
+	err := _Ed25519.contract.Call(opts, out, "scalarmult", s)
+	return *ret0, *ret1, err
+}
+
+// Scalarmult is a free data retrieval call binding the contract method 0xe49cf911.
+//
+// Solidity: function scalarmult(uint256 s) constant returns(uint256, uint256)
+func (_Ed25519 *Ed25519Session) Scalarmult(s *big.Int) (*big.Int, *big.Int, error) {
+	return _Ed25519.Contract.Scalarmult(&_Ed25519.CallOpts, s)
+}
+
+// Scalarmult is a free data retrieval call binding the contract method 0xe49cf911.
+//
+// Solidity: function scalarmult(uint256 s) constant returns(uint256, uint256)
+func (_Ed25519 *Ed25519CallerSession) Scalarmult(s *big.Int) (*big.Int, *big.Int, error) {
+	return _Ed25519.Contract.Scalarmult(&_Ed25519.CallOpts, s)
+}
--- testsuite.go
+++ testsuite.go
@@ -5,34 +5,41 @@ import (
 	"crypto/ecdsa"
 	"crypto/rand"
 	"encoding/hex"
+	"flag"
 	"fmt"
 	"log"
 	"math/big"
 
-	"golang.org/x/crypto/curve25519"
+	"github.com/HyperspaceApp/ed25519"
 
+	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 
-	solidityCurve25519 "./contract"
+	solidityEd25519 "./contract"
 )
 
-// Start Ganache with:
-// ganache-cli --account \
-//   "0xa1d63a5f23ac9b62199e84d87fff196c603b61f6c42bddd0bcca9839d7449ba7,100000000000000000000"
 const GanacheEndpoint = "http://127.0.0.1:8545"
 const MainPrivateKey = "a1d63a5f23ac9b62199e84d87fff196c603b61f6c42bddd0bcca9839d7449ba7"
 
 func toBigEndian(littleEndian []byte) []byte {
 	bigEndian := make([]byte, len(littleEndian))
-	for i := 0; i < len(littleEndian); i++ {
+	for i := range littleEndian {
 		bigEndian[i] = littleEndian[len(littleEndian)-1-i]
 	}
 	return bigEndian
 }
 
 func main() {
+	var n = flag.Int("n", 3, "number of test cases to generate")
+	flag.Parse()
+
+	fmt.Printf("Please ensure Ganache has been started with this command:\n")
+	fmt.Printf("  ganache-cli --account \"0x%s,100000000000000000000\"\n", MainPrivateKey)
+
 	// Connect to local Ethereum testnet and prepare smart contract deployment
 	client, err := ethclient.Dial(GanacheEndpoint)
 	if err != nil {
@@ -66,34 +73,67 @@ func main() {
 	auth := bind.NewKeyedTransactor(privateKey)
 	auth.Nonce = big.NewInt(int64(nonce))
 	auth.Value = big.NewInt(0)
-	auth.GasLimit = uint64(500000)
+	auth.GasLimit = uint64(6700000)
 	auth.GasPrice = gasPrice
 
-	_, _, instance, err := solidityCurve25519.DeployCurve25519(auth, client)
+	address, _, instance, err := solidityEd25519.DeployEd25519(auth, client)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Pick a random key and compare Go's curve25519
-	// implementation with the Solidity code.
-	var in, dst [32]byte
-	_, err = rand.Read(in[:])
-	if err != nil {
-		log.Fatal(err)
+	for i := 0; i < *n; i++ {
+		// Create random keypair and compare results from Go and Solidity
+		adaptor, adaptorPoint, err := ed25519.GenerateAdaptor(rand.Reader)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		adaptorBigInt := new(big.Int)
+		adaptorBigInt.SetBytes(toBigEndian(adaptor[:]))
+		adaptorPointBigInt := new(big.Int)
+		adaptorPointBytes := toBigEndian(adaptorPoint[:])
+		adaptorPointBytes[0] &= 127 // clear sign bit
+		adaptorPointBigInt.SetBytes(adaptorPointBytes)
+
+		_, adaptorPointSolidity, err := instance.Scalarmult(nil, adaptorBigInt)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var estimateData []byte
+		estimateData = append(estimateData, hexutil.MustDecode("0xe49cf911")...) // scalarmult
+		estimateData = append(estimateData, common.LeftPadBytes(adaptorBigInt.Bytes(), 32)...)
+
+		gasEstimate, err := client.EstimateGas(context.Background(), ethereum.CallMsg{
+			To:   &address,
+			Data: estimateData,
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		wasSuccessful := adaptorPointBigInt.Cmp(adaptorPointSolidity) == 0
+		if wasSuccessful {
+			fmt.Printf("\nTest successful:\n")
+		} else {
+			fmt.Printf("\nTest failed:\n")
+		}
+		fmt.Printf("  Adaptor (little endian)                   : %s\n",
+			hex.EncodeToString(adaptor[:]))
+		fmt.Printf("  Adaptor (big int, hex)                    : %s\n",
+			adaptorBigInt.Text(16))
+		fmt.Printf("  Adaptor (big int, decimal)                : %s\n",
+			adaptorBigInt.Text(10))
+		fmt.Printf("\n")
+		fmt.Printf("  Adaptor point (big int, decimal, Go)      : %s\n",
+			adaptorPointBigInt.Text(10))
+		fmt.Printf("  Adaptor point (big int, decimal, Solidity): %s\n",
+			adaptorPointSolidity.Text(10))
+		fmt.Printf("\n")
+		fmt.Printf("  Gas estimate: %d\n", gasEstimate)
+
+		if !wasSuccessful {
+			log.Fatal("last test failed")
+		}
 	}
-	curve25519.ScalarBaseMult(&dst, &in)
-
-	solidityIn := new(big.Int)
-	solidityIn.SetBytes(toBigEndian(in[:]))
-
-	solidityDst, err := instance.PublicKey(nil, solidityIn)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Printf("Tested %s (big endian)\n", solidityIn.Text(16))
-	fmt.Printf("  Go code      : %s\n", hex.EncodeToString(dst[:]))
-	fmt.Printf("  Go code      : %s\n", hex.EncodeToString(toBigEndian(dst[:])))
-	fmt.Printf("  Solidity code: %s\n", solidityDst.Qx.Text(16))
-	fmt.Printf("  Solidity code: %s\n", solidityDst.Qy.Text(16))
 }
